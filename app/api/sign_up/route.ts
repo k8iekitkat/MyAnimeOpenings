65ab0de5851e6b18af5662c1e935e76fede92c01
Kaitlyn Jao <kaitlynjao@csu.fullerton.edu>
2026-04-04 19:24:37 -0700

    signup api boilerplate

---
+++ app/api/sign_up/route.ts
@@ -0,0 +1,42 @@
+type SignupBody = {
+    user_name: string;
+    password: string;
+}
+
+
+export async function POST(request: Request) {
+    let body: SignupBody;
+
+    try {
+        body = await request.json();
+    } catch {
+        return Response.json(
+            { error: "Invalid JSON body" },
+            { status: 400 }
+        );
+    }
+
+    const { user_name, password } = body;
+
+    if (!user_name || !password) {
+        return Response.json(
+            { error: "Missing required fields" },
+            { status: 400 }
+        );
+    }
+
+    if (password.length < 8) {
+        return Response.json(
+            { error: "Password must be at least 8 characters" },
+            { status: 400 }
+        );
+    };
+
+   // TODO: check DB, hash password, create user 
+
+   return Response.json(
+    { message: "User created" },
+    { status: 201 }
+   );
+}
+
