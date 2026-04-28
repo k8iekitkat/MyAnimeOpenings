adc2f6eebc9cf6b5733b07825484691e3e15987d
Kaitlyn Jao <kaitlynjao@csu.fullerton.edu>
2026-04-27 22:45:42 -0700

    routed from login to account profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,16 +3,30 @@ import { redirect } from "next/navigation";
 import { supabase } from '@/lib/supabase';
 import { cookies } from "next/headers";
 
+
 async function getUserFromSession() {
+  const cookieStore = await cookies();
+  const session_token = cookieStore.get('session')?.value;
+  if (!session_token) {
+    redirect("/signup");
+  }
+  const {data, error} = await supabase.from('sessions').select('username').eq('cookie_id', session_token);
+  if (error) {
+    redirect("/signup");
+  }
+  // const username = data[0].username;
+  // const { data, error } = await supabase.from('users').select('*').eq('username', username);
+  return data[0];
 }
 
-export default function ProfilePage() {
-  const user = getUserFromSession();
+export default async function ProfilePage() {
+  const user = await getUserFromSession();
   if(!user) {
+    console.log(user);
     redirect('/login');
   }
   const userProfile = {
-    username: "temp_user",
+    username: user.username,
   };
 
   const highestRatedTheme = {
